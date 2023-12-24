735b2b3bcccd19d72dcc6543f4ead8ee945f88a5
Aluma Gelbard <10659903+alumag@users.noreply.github.com>
2023-12-24 11:30:37 +0200

    tailwind: theme.extend
    Signed-off-by: Aluma Gelbard <10659903+alumag@users.noreply.github.com>

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,4 @@
 import type { Config } from 'tailwindcss'
-import colors from 'tailwindcss/colors';
 
 const config: Config = {
   darkMode: 'class',
@@ -7,20 +6,19 @@ const config: Config = {
     './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    colors: {
-      ...colors,
-      app: {
-        pink: '#dfd1e0',
-        purple: '#aab8c8',
-        blue: '#c9e4e2',
-      }
-    },
-    fontFamily: {
-      alef: ['Alef', 'var(--font-alef)'],
-      rubik: ['Rubik', 'var(--font-rubik)'],
-      sans: ['Inter', 'var(--font-default)'],
-    },
     extend: {
+      colors: {
+        app: {
+          pink: '#dfd1e0',
+          purple: '#aab8c8',
+          blue: '#c9e4e2',
+        }
+      },
+      fontFamily: {
+        alef: ['Alef', 'var(--font-alef)'],
+        rubik: ['Rubik', 'var(--font-rubik)'],
+        sans: ['Inter', 'var(--font-default)'],
+      },
     },
   },
   plugins: [
