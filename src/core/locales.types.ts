e30dc6238811df6e624409b47590b719cfbd0f01
Aluma Gelbard <10659903+alumag@users.noreply.github.com>
2023-12-31 18:10:48 +0200

    prettier
    Signed-off-by: Aluma Gelbard <10659903+alumag@users.noreply.github.com>

--- src/core/locales.types.ts
+++ src/core/locales.types.ts
@@ -1,3 +1,3 @@
 export const LOCALES = ["he", "en"] as const;
 export const DEFAULT_LOCALE = "he" as const;
-export type Locale = typeof LOCALES[number];
+export type Locale = (typeof LOCALES)[number];
