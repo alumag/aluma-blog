40b5a6498fb15f41f91a4291ae861f33e0c4ee1d
Aluma Gelbard <10659903+alumag@users.noreply.github.com>
2024-01-11 21:09:08 +0200

    locales: set sub domains
    Signed-off-by: Aluma Gelbard <10659903+alumag@users.noreply.github.com>

--- src/core/getLocaleCookie.ts
+++ src/core/getLocaleCookie.ts
@@ -1,7 +1,12 @@
-import { cookies } from "next/headers";
-import { Locale, DEFAULT_LOCALE } from "./locales.types";
+import { cookies, headers } from "next/headers";
+import { Locale, DEFAULT_LOCALE, LOCALES } from "./locales.types";
 
 export const getLocaleCookie = (): Locale => {
+  const subdomain = headers().get("host")?.split('.')[0] as Locale;
+  if (LOCALES.includes(subdomain)) {
+    return subdomain;
+  }
+
   const cookieStore = cookies();
   const locale = cookieStore.get("locale");
 
