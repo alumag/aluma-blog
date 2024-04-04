a66b538c4174238a5ea3b83788ed4b2f65ebc03b
Aluma Gelbard <10659903+alumag@users.noreply.github.com>
2024-04-04 17:22:15 +0300

    Index sitemap
    Signed-off-by: Aluma Gelbard <10659903+alumag@users.noreply.github.com>

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,21 +1,25 @@
 import { getClient } from '@/lib/sanity.client';
 import { getPost, getPostsSitemap } from '@/lib/sanity.queries';
 import { MetadataRoute } from 'next'
+import { headers } from "next/headers";
+import { Locale } from "@/core/locales.types";
 
 const client = getClient();
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const posts = await getPostsSitemap(client, "craft");
   const index = await getPost(client, "aluma-gelbard");
+  const subdomain = headers().get("host")?.split('.')[0] as Locale;
+  const baseUrl = subdomain === "he" ? "https://he.alumagelbard.com" : "https://alumagelbard.com";
 
   return [
     {
-      url: '/',
+      url: `${baseUrl}/`,
       lastModified: index._updatedAt,
       changeFrequency: "monthly",
     },
     {
-      url: '/craft',
+      url: `${baseUrl}/craft`,
       lastModified: posts[0]?._updatedAt,
       changeFrequency: "weekly",
     }
