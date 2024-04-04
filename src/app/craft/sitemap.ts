186d6326c3ed7d6b5e3b62eb91feed0275c24987
Aluma Gelbard <10659903+alumag@users.noreply.github.com>
2024-04-04 17:21:06 +0300

    Craft sitemap
    Signed-off-by: Aluma Gelbard <10659903+alumag@users.noreply.github.com>

--- src/app/craft/sitemap.ts
+++ src/app/craft/sitemap.ts
@@ -1,14 +1,18 @@
 import { getClient } from '@/lib/sanity.client';
 import { getPostsSitemap } from '@/lib/sanity.queries';
 import { MetadataRoute } from 'next'
+import { headers } from "next/headers";
+import { Locale } from "@/core/locales.types";
 
 const client = getClient();
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const posts = await getPostsSitemap(client, "craft");
+  const subdomain = headers().get("host")?.split('.')[0] as Locale;
+  const baseUrl = subdomain === "he" ? "https://he.alumagelbard.com" : "https://alumagelbard.com";
 
   return posts.map((post) => ({
-      url: `/craft/${post.slug.current}`,
+      url: `${baseUrl}/craft/${post.slug.current}`,
       lastModified: post._updatedAt,
     }));
 }
