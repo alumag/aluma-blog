5135268487a1cb142eb1e8de495349b9b54febad
Aluma Gelbard <10659903+alumag@users.noreply.github.com>
2024-04-04 14:47:10 +0300

    > disable locale cookie
    Signed-off-by: Aluma Gelbard <10659903+alumag@users.noreply.github.com>

--- src/core/getLocaleCookie.ts
+++ src/core/getLocaleCookie.ts
@@ -1,17 +1,10 @@
-import { cookies, headers } from "next/headers";
+import { headers } from "next/headers";
 import { Locale, DEFAULT_LOCALE, LOCALES } from "./locales.types";
 
 export const getLocaleCookie = (): Locale => {
   const subdomain = headers().get("host")?.split('.')[0] as Locale;
   if (LOCALES.includes(subdomain)) {
     return subdomain;
-  }
-
-  const cookieStore = cookies();
-  const locale = cookieStore.get("locale");
-
-  if (locale) {
-    return locale.value as Locale;
   }
 
   return DEFAULT_LOCALE;
