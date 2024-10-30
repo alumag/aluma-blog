df48e342bea1a13b8c1cecf87b1a77df1651c6df
Aluma Gelbard <10659903+alumag@users.noreply.github.com>
2024-10-30 15:47:19 +0200

    fix locale
    Signed-off-by: Aluma Gelbard <10659903+alumag@users.noreply.github.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,47 +1,44 @@
-import { type Metadata } from "next";
-import { GoogleTagManager } from "@next/third-parties/google";
-import { SpeedInsights } from "@vercel/speed-insights/next";
-import { alefFont, defaultFont, rubikFont } from "@/fonts";
-import { gtmId } from "@/lib/gtm";
-import "./globals.css";
-import { client } from "@/sanity/lib/client";
-import { getSiteMetadataQuery } from "@/sanity/lib/queries";
-import { notFound } from "next/navigation";
+import {type Metadata} from 'next';
+import {GoogleTagManager} from '@next/third-parties/google';
+import {SpeedInsights} from '@vercel/speed-insights/next';
+import {alefFont, defaultFont, rubikFont} from '@/fonts';
+import {gtmId} from '@/lib/gtm';
+import './globals.css';
+import {client} from '@/sanity/lib/client';
+import {getSiteMetadataQuery} from '@/sanity/lib/queries';
+import {notFound} from 'next/navigation';
+import {getLocale} from '@/core/getLocale';
 
 export async function generateMetadata(): Promise<Metadata> {
-  const data = await client.fetch(getSiteMetadataQuery, { language: "he" });
+  const data = await client.fetch(getSiteMetadataQuery, {language: getLocale()});
   if (data === null) {
     notFound();
   }
 
   return {
     metadataBase:
-      data.language === "he"
-        ? new URL("https://he.alumagelbard.com")
-        : new URL("https://alumagelbard.com"),
+      data.language === 'he'
+        ? new URL('https://he.alumagelbard.com')
+        : new URL('https://alumagelbard.com'),
     title: {
       template: `${data.title} / %s`,
       default: data.title, // a default is required when creating a template
     },
     description: data.description,
     openGraph: {
-      type: "website",
+      type: 'website',
       locale: data.locale,
-      countryName: "Israel",
+      countryName: 'Israel',
       siteName: data.siteName,
       title: data.siteName,
       description: data.description,
-      images: "/images/20230521_110544.jpg",
+      images: '/images/20230521_110544.jpg',
     },
   };
 }
 
-export default async function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  const data = await client.fetch(getSiteMetadataQuery, { language: "he" });
+export default async function RootLayout({children}: {children: React.ReactNode}) {
+  const data = await client.fetch(getSiteMetadataQuery, {language: getLocale()});
   if (data === null) {
     notFound();
   }
